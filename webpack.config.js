cd8e62097d28627a76dcdb469d73bfba31df0d4b
eveporcello <eve@moonhighway.com>
2016-05-21 10:07:50 -0700

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    entry: "./index.js",
+    output: {
+        path: "dist/assets",
+        filename: "bundle.js",
+        publicPath: "/assets/"
+    },
+    devServer: {
+        inline: true,
+        contentBase: './dist',
+        port: 3333
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loader: ['babel'],
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
