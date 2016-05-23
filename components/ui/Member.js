e7d7f75db6dff3d91173d031c025611daad5a944
Alex Banks <alex@moonhighway.com>
2016-05-22 18:55:22 -0700

    Adds MemberInfo and Member component for Membership Page

---
+++ components/ui/Member.js
@@ -0,0 +1,23 @@
+import { PropTypes } from 'react'
+import {capatalize, getStateAbbrev} from '../../lib'
+
+const Member = ({id, name, email, cell, picture, location}) => (
+    <div className={`member-${id}`}>
+        <h1>{capatalize(name.last)}, {capatalize(name.first)}</h1>
+        <img src={picture.thumbnail} alt="profile picture" />
+        <p>{capatalize(location.city)}, {getStateAbbrev(location.state)}</p>
+        <p><a href={`mailto:${email}`}>{email}</a></p>
+        <p>{cell}</p>
+    </div>
+)
+
+Member.propTypes = {
+    id: PropTypes.array.isRequired,
+    name: PropTypes.object.isRequired,
+    email: PropTypes.string.isRequired,
+    cell: PropTypes.string.isRequired,
+    picture: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired
+}
+
+module.exports = Member
---
+++ components/ui/MemberInfo.js
@@ -0,0 +1,25 @@
+import { PropTypes } from 'react'
+import {compose, filterByGender, filterByState} from '../../lib'
+
+const MemberInfo = ({members, gender="any", state="any"}) => {
+
+    const membersToRender = compose(
+        filterByGender(gender.toLowerCase()),
+        filterByState(state.toLowerCase())
+    )(members);
+
+    return (
+        <div className="member-info">
+            <h1>{membersToRender.length} {(gender==="any") ? "Members" : (gender==="female") ? "Women" : "Men"}</h1>
+            {membersToRender.map((data, i) => <Member key={i} {...data} />)}
+        </div>
+    )
+}
+
+MemberInfo.propTypes = {
+    members: PropTypes.array.isRequired,
+    gender: PropTypes.string,
+    state: PropTypes.string
+}
+
+module.exports = MemberInfo
